aef6ceb161d9091c604885413930abe79510ef51
Abdelrahman <abdelrahman.ashraf.engineer@gmail.com>
2026-05-02 14:58:23 +0300

    Initial release of Field Highlight with premium description

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Field Highlight",
+    "summary": "Global UI enhancement providing clear field visibility and focus across all Odoo form views.",
+    "version": "19.0.1.0.0",
+    "category": "User Interface",
+    "author": "Abdelrahman Ashraf",
+    "website": "https://www.linkedin.com/in/abdelrahman-ashraf-ahmed-ahmed/",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "field_highlight/static/src/css/field_border.scss",
+        ],
+    },
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "application": False,
+    "price": 0,
+    "currency": "EUR",
+}
